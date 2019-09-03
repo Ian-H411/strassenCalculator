5b9fcebbff26ffe19bf53de3924411f3c802d07d
Ian_H411 <ian.h411@gmail.com>
2019-09-03 16:02:43 -0600

    finished UI and bug fixes

--- strassen/strassen/Views/CalculatorViewController.swift
+++ strassen/strassen/Views/CalculatorViewController.swift
@@ -9,7 +9,28 @@
 import UIKit
 
 class CalculatorViewController: UIViewController, UITextFieldDelegate {
+    //MARK: - OUTLETS
     //numberButtons
+    
+    @IBOutlet weak var num1: UIButton!
+    
+    @IBOutlet weak var num2: UIButton!
+    
+    @IBOutlet weak var num3: UIButton!
+    
+    @IBOutlet weak var num4: UIButton!
+    
+    @IBOutlet weak var num5: UIButton!
+    
+    @IBOutlet weak var num6: UIButton!
+    
+    @IBOutlet weak var num7: UIButton!
+    
+    @IBOutlet weak var num8: UIButton!
+    
+    @IBOutlet weak var num9: UIButton!
+    
+    @IBOutlet weak var num0: UIButton!
     
     //matrixA
     @IBOutlet weak var cell1: UITextField!
@@ -20,8 +41,11 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var cell4: UITextField!
     
+    //delete and clear
+    
     @IBOutlet weak var deleteButton: UIButton!
     
+    @IBOutlet weak var clearTablesButton: UIButton!
     //matrixB
     
     @IBOutlet weak var cell5: UITextField!
@@ -50,15 +74,19 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
     
     //a container for all cells
     
-    
+    //MARK: - LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpUI()
         
     }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     func setUpUI(){
-        var cellCycle = [cell1,cell2,cell3,cell4,cell5,cell6,cell7,cell8, answerCell1, answerCell2, answerCell3, answerCell4]
+        let cellCycle = [cell1,cell2,cell3,cell4,cell5,cell6,cell7,cell8, answerCell1, answerCell2, answerCell3, answerCell4]
+        let buttonCycle = [num0,num1,num2,num3,num4,num5,num6,num7,num8,num9,num0]
         UIView.animate(withDuration: 6.0, delay: 0.0, options:[.repeat, .transitionFlipFromBottom, .autoreverse, .allowUserInteraction], animations: {
             self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
         }, completion:nil)
@@ -66,20 +94,32 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
             cell?.backgroundColor = .clear
             cell?.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             cell?.layer.borderWidth = 1.0
+            cell?.layer.cornerRadius = 10
         }
-        
+        clearTablesButton.setTitle("", for: .normal)
+        for numberButton in buttonCycle{
+            numberButton?.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            numberButton?.layer.borderWidth = 1.0
+            numberButton?.layer.cornerRadius = 10
+        }
     }
-    
+    //MARK: - ACTIONS
     @IBAction func numberButtonTapped(_ sender: Any) {
+        
         guard let number = (sender as AnyObject).tag else {return}
         var cellCycle = [cell1,cell2,cell3,cell4,cell5,cell6,cell7,cell8]
         if currentCell < cellCycle.count{
             cellCycle[currentCell]?.text = "\(number)"
             currentCell = currentCell + 1
+            if currentCell == 1 {
+                clearTablesButton.setTitle("Clear Tables", for: .normal)
+            }
         }
         if currentCell == 8{
             calculate()
+            clearTablesButton.setTitle("", for: .normal)
         }
+        
     }
     
     @IBAction func deleteButtonTapped(_ sender: Any) {
@@ -95,13 +135,23 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
             currentCell = currentCell - 1
             if currentCell < 0{
                 currentCell = 0
+            } else if currentCell == 0{
+                clearTablesButton.setTitle("", for: .normal)
             }
             cellCycle[currentCell]?.text = ""
         }
     }
     
+    @IBAction func clearTableButtonTapped(_ sender: Any) {
+         let cellCycle = [cell1,cell2,cell3,cell4,cell5,cell6,cell7,cell8, answerCell1, answerCell2, answerCell3, answerCell4]
+            for cell in cellCycle{
+                cell?.text = ""
+        }
+        currentCell = 0
+        clearTablesButton.setTitle("", for: .normal)
+    }
     
-    
+    //MARK: - HELPER FUNCTIONS
     func calculate() {
         let cellCycle = [cell1,cell2,cell3,cell4,cell5,cell6,cell7,cell8]
         var intArray:[Int] = []
@@ -129,6 +179,7 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
         answerCell3.text = "\(answerArray[2])"
         answerCell4.text = "\(answerArray[3])"
         deleteButton.setTitle("ClearTables", for: .normal)
+        
         clearTableMode = true
         
     }
